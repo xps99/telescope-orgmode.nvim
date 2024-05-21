1d080c3e5d713403c2cea631295d2b7734397525
Sebastian Flügge <sebastian.fluegge@dnv.com>
2024-05-21 02:14:22 +0200

    refactor: split large function
    To remove variable shadowing and make the code easier to work with,
    migrate the local methods into a class and return a callback, which only
    contains the picker itself.

--- lua/telescope-orgmode/refile_heading.lua
+++ lua/telescope-orgmode/refile_heading.lua
@@ -10,53 +10,57 @@ local utils = require('telescope-orgmode.utils')
 
 local api = require('orgmode.api')
 
+local M = {}
+
+M.refile = function(prompt_bufnr)
+  local entry = action_state.get_selected_entry()
+  actions.close(prompt_bufnr)
+
+  -- Refile to the file by default
+  local destination = entry.value.file
+
+  -- Refile to a specific heading if is set
+  if entry.value.headline then
+    destination = entry.value.headline
+  end
+
+  return api.refile({
+    source = M.closest_headline,
+    destination = destination,
+  })
+end
+
+M.gen_depth_toggle = function(opts, prompt_bufnr)
+  local status = state.get_status(prompt_bufnr)
+  status._ot_current_depth = opts.max_depth
+  status._ot_next_depth = nil
+  if status._ot_current_depth ~= 0 then
+    status._ot_next_depth = 0
+  end
+
+  return function()
+    local current_picker = action_state.get_current_picker(prompt_bufnr)
+
+    local aux = status._ot_current_depth
+    status._ot_current_depth = status._ot_next_depth
+    status._ot_next_depth = aux
+
+    opts.max_depth = status._ot_current_depth
+    local new_finder = finders.new_table({
+      results = utils.get_entries(opts),
+      entry_maker = opts.entry_maker or utils.make_entry(opts),
+    })
+
+    current_picker:refresh(new_finder, opts)
+  end
+end
+
+M.closest_headline = nil
+
 return function(opts)
   opts = opts or {}
 
-  local closest_headline = api.current():get_closest_headline()
-
-  local function refile(prompt_bufnr)
-    local entry = action_state.get_selected_entry()
-    actions.close(prompt_bufnr)
-
-    -- Refile to the file by default
-    local destination = entry.value.file
-
-    -- Refile to a specific heading if is set
-    if entry.value.headline then
-      destination = entry.value.headline
-    end
-
-    return api.refile({
-      source = closest_headline,
-      destination = destination,
-    })
-  end
-
-  local function gen_depth_toggle(opts, prompt_bufnr)
-    local status = state.get_status(prompt_bufnr)
-    status._ot_current_depth = opts.max_depth
-    status._ot_next_depth = nil
-    if status._ot_current_depth ~= 0 then
-      status._ot_next_depth = 0
-    end
-
-    return function()
-      local current_picker = action_state.get_current_picker(prompt_bufnr)
-
-      local aux = status._ot_current_depth
-      status._ot_current_depth = status._ot_next_depth
-      status._ot_next_depth = aux
-
-      opts.max_depth = status._ot_current_depth
-      local new_finder = finders.new_table({
-        results = utils.get_entries(opts),
-        entry_maker = opts.entry_maker or utils.make_entry(opts),
-      })
-
-      current_picker:refresh(new_finder, opts)
-    end
-  end
+  M.closest_headline = api.current():get_closest_headline()
 
   pickers
     .new(opts, {
@@ -70,8 +74,8 @@ return function(opts)
       sorter = conf.generic_sorter(opts),
       previewer = conf.grep_previewer(opts),
       attach_mappings = function(prompt_bufnr, map)
-        action_set.select:replace(refile)
-        map('i', '<c-space>', gen_depth_toggle(opts, prompt_bufnr))
+        action_set.select:replace(M.refile)
+        map('i', '<c-space>', M.gen_depth_toggle(opts, prompt_bufnr))
         return true
       end,
     })
