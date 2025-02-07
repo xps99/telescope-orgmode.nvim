7bfa3fbe9155ee9e9112a803989457b32e962d01
xps <xps@lenny>
2025-02-07 22:12:00 +0100

    zx after search

--- lua/telescope-orgmode/picker/search_headings.lua
+++ lua/telescope-orgmode/picker/search_headings.lua
@@ -5,11 +5,14 @@ local conf = require('telescope.config').values
 local config = require('telescope-orgmode.config')
 local mappings = require('telescope-orgmode.mappings')
 
+local action_set = require('telescope.actions.set') -- <--- ADD THIS
+local actions = require('telescope.actions') -- <--- AND THIS
+
 return function(opts)
   opts = config.init_opts(opts, {
     headlines = 'Search headlines',
     orgfiles = 'Search org files',
-  }, "headlines")
+  }, 'headlines')
 
   pickers
     .new(opts, {
@@ -18,6 +21,16 @@ return function(opts)
       sorter = conf.generic_sorter(opts),
       previewer = conf.grep_previewer(opts),
       attach_mappings = function(_, map)
+        -- Override the default select action to add fold reordering
+        action_set.select:enhance({
+          post = function()
+            -- Run zx after jumping to the selected entry
+            vim.schedule(function()
+              vim.cmd('normal! zx')
+            end)
+          end,
+        })
+
         mappings.attach_mappings(map, opts)
         return true
       end,
